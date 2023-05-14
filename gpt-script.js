97db066072aab956161a4e4106ae1d340c5da183
josephlu2 <jjl55@duke.edu>
2023-05-13 21:56:59 -0400

    16

--- gpt-script.js
+++ gpt-script.js
@@ -4,7 +4,7 @@ icon.style.position = 'fixed';
 icon.style.bottom = '20px';
 icon.style.right = '20px';
 icon.style.zIndex = '9999';
-icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://cdn.dialoqai.com/js/dialoq/images/chat.svg">';
+icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://www.nicepng.com/png/detail/335-3354103_home-icon-button-png-green-online-icon-png.png">';
 
 // Append the icon to the document body
 document.body.appendChild(icon);
@@ -23,13 +23,25 @@ icon.addEventListener('click', function () {
   iframe.style.zIndex = '10000';
 
   // Replace the icon with the close button and append the iframe to the document body
-  icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://cdn.dialoqai.com/js/dialoq/images/chat.svg">';
+  icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://upload.wikimedia.org/wikipedia/commons/2/29/Red_button.png">';
   document.body.appendChild(iframe);
 
   // Listen for click events on the close button
   icon.addEventListener('click', function () {
     // Remove the iframe and replace the close button with the original icon
     document.body.removeChild(iframe);
-    icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://cdn.dialoqai.com/js/dialoq/images/chat.svg">';
+    icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://www.nicepng.com/png/detail/335-3354103_home-icon-button-png-green-online-icon-png.png">';
+  });
+
+  // Listen for click events on the iframe
+  iframe.addEventListener('click', function (event) {
+    // Check if the click event originated from inside the iframe
+    if (event.target !== iframe) {
+      return;
+    }
+
+    // Close the iframe and replace the close button with the original icon
+    document.body.removeChild(iframe);
+    icon.innerHTML = '<img style="width: 50px; height: 50px; cursor: pointer;" src="https://www.nicepng.com/png/detail/335-3354103_home-icon-button-png-green-online-icon-png.png">';
   });
 });
